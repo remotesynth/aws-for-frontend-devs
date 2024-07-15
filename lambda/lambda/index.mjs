4261af37a1c8306ffe2371be390a9ffb473fdc95
Brian Rinaldi <brian.rinaldi@gmail.com>
2024-07-15 13:17:45 -0400

    fix cors in Lambda function URL
    the extra headers aren't necessary as the function URL adds them automatically.

--- lambda/lambda/index.mjs
+++ lambda/lambda/index.mjs
@@ -3,12 +3,6 @@ export const handler = async (event) => {
   // return a response once it is completed
   const response = {
     statusCode: 200,
-    headers: {
-      'Access-Control-Allow-Headers':
-        'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token',
-      'Access-Control-Allow-Methods': '*',
-      'Access-Control-Allow-Origin': '*',
-    },
     body: {
       message: 'You successfully called the Lambda from a function URL!',
     },
